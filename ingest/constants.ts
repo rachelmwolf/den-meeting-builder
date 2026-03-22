e92bd38911428a6e5556a283f52e6b96069ec0a0
rachelmwolf <wolf.rachelmarie@gmail.com>
2026-03-22 10:57:51 -0600

    Add ingest/constants.ts

---
+++ ingest/constants.ts
@@ -0,0 +1,42 @@
+export const BASE_URL = "https://www.scouting.org";
+
+export const rankIndexUrl = `${BASE_URL}/programs/cub-scouts/adventures/`;
+
+export const rankOverrides = [
+  {
+    name: "Lion",
+    grade: "Kindergarten",
+    slug: "lion",
+    sourceUrl: `${BASE_URL}/programs/cub-scouts/adventures/lion/`
+  },
+  {
+    name: "Tiger",
+    grade: "1st Grade",
+    slug: "tiger",
+    sourceUrl: `${BASE_URL}/programs/cub-scouts/adventures/tiger/`
+  },
+  {
+    name: "Wolf",
+    grade: "2nd Grade",
+    slug: "wolf",
+    sourceUrl: `${BASE_URL}/programs/cub-scouts/adventures/wolf/`
+  },
+  {
+    name: "Bear",
+    grade: "3rd Grade",
+    slug: "bear",
+    sourceUrl: `${BASE_URL}/programs/cub-scouts/adventures/bear/`
+  },
+  {
+    name: "Webelos",
+    grade: "4th Grade",
+    slug: "webelos",
+    sourceUrl: `${BASE_URL}/programs/cub-scouts/adventures/webelos/`
+  },
+  {
+    name: "Arrow of Light",
+    grade: "5th Grade",
+    slug: "arrow-of-light",
+    sourceUrl: `${BASE_URL}/programs/cub-scouts/adventures/arrow-of-light/`
+  }
+];
